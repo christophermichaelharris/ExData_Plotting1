2d9879ebb3cac714b4d12c5626948b4dfa961d17
Chris <47044369+christophermichaelharris@users.noreply.github.com>
2019-03-06 20:48:51 -0500

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -12,13 +12,20 @@ if(!file.exists(filename)){      ##checks if file has already been downloaded
   unzip(filename)
   data <- read.table("household_power_consumption.txt",sep=";")
   data <- data[-1,]
-  colnames(data) <- c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","SubMetering1","SubMetering2","SubMetering3")
+  colnames(data) <- c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","Sub_Metering1","Sub_Metering2","Sub_Metering3")
   data$Date <- as.Date(data$Date, "%d/%m/%Y")
   observations <- subset(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
   
 }
 
-kWGAP <- as.numeric(as.character(observations$GlobalActivePower))   ##Creates variable for the plot
 datetime <- as.POSIXct(paste(observations$Date,observations$Time))
-plot(datetime,kWGAP,col="white",ylab = "Global Active Power (kilowatts")
-lines(datetime,kWGAP)
+SM1 <- as.numeric(as.character(observations$SubMetering1))   ##Creates a new variable for the plot 
+SM2 <- as.numeric(as.character(observations$SubMetering2))   ##Creates a new variable for the plot 
+SM3 <- as.numeric(as.character(observations$SubMetering3))   ##Creates a new variable for the plot 
+
+plot(datetime,SM1,col="white")
+lines(datetime,SM1, col="red")
+points(datetime,SM2,col="white")
+lines(datetime,SM1, col="blue")
+points(datetime,SM3,col="white")
+lines(datetime,SM3, col="black")
